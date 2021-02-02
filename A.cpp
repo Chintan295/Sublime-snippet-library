3450edace355b6efe31511f8cccc010c17da69fa
Chintan295 <chintansavaliya2000@gmail.com>
2021-02-02 19:13:32 +0530

    initial commit

---
+++ A.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define M 1000000007
+using namespace std;
+ll n,t,x,y,m;
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    cin>>n;
+    cout<<powermod(2,7);
+}
